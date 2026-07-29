29f12991ba3d17bd448205c99b42a55a4feb9514
Shreyas Immadi <shreyas.immadi@gmail.com>
2026-07-29 00:24:46 -0400

    Fixed scope of gmail from readonly to modify

--- backend/services/auth.py
+++ backend/services/auth.py
@@ -5,7 +5,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 SCOPES = [
-    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.modify',
     'https://www.googleapis.com/auth/spreadsheets'
 ]
 
